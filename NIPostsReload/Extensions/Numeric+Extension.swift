42fac4010a8edcab503d510b97f74df2c1d4baaa
Denys Niestierov <niestierov.denys.natife@gmail.com>
2023-12-06 16:29:40 +0200

    added numeric extension

---
+++ NIPostsReload/Extensions/Numeric+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Numeric+Extension.swift
+//  NIPostsReload
+//
+//  Created by Denys Niestierov on 06.12.2023.
+//
+
+import Foundation
+
+extension Numeric {
+    var stringValue: String {
+        return String(describing: self)
+    }
+}
