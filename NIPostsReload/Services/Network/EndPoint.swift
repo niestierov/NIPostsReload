ad99f8620aa9eebfd20e26843b1fa525413760fe
Denys Niestierov <niestierov.denys.natife@gmail.com>
2023-12-18 11:48:50 +0200

    updated endpoint

--- NIPostsReload/Services/Network/EndPoint.swift
+++ NIPostsReload/Services/Network/EndPoint.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+import Alamofire
+
+private struct EndPointConstant {
+    static let baseUrl = "https://raw.githubusercontent.com"
+    static let jsonFormat = ".json"
+    
+    enum UrlPath {
+        case list
+        case details(id: Int)
+        
+        var component: String {
+            switch self {
+            case .list:
+                return "/anton-natife/jsons/master/api/main.json"
+            case .details:
+                return "/anton-natife/jsons/master/api/posts/"
+            }
+        }
+    }
+}
+
+enum EndPoint {
+    case list
+    case details(id: Int)
+    
+    var urlString: String {
+        switch self {
+        case .list, .details:
+            return EndPointConstant.baseUrl + path
+        }
+    }
+    
+    var url: URL? {
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        return url
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .list,
+             .details:
+            return .get
+        }
+    }
+    
+    var encoding: ParameterEncoding {
+        switch self {
+        case .list,
+             .details:
+            return URLEncoding.default
+        }
+    }
+    
+    // MARK: - Private -
+    
+    private var path: String {
+        switch self {
+        case .list:
+            return EndPointConstant.UrlPath.list.component
+        case .details(let id):
+            return EndPointConstant.UrlPath.details(id: id).component + String(id) + EndPointConstant.jsonFormat
+        }
+    }
+}
--- NIPostsReload/Services/Network/NetworkService.swift
+++ NIPostsReload/Services/Network/NetworkService.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol NetworkService {
+    func request<T: Codable>(
+        endPoint: EndPoint,
+        type: T.Type,
+        completion: @escaping (Result<T?, Error>) -> Void
+    )
+}
+
+final class DefaultNetworkService: NetworkService {
+    
+    //MARK: - Internal -
+    
+    func request<T: Codable>(
+        endPoint: EndPoint,
+        type: T.Type,
+        completion: @escaping (Result<T?, Error>) -> Void
+    ) {
+        guard let url = endPoint.url else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        AF.request(
+            url,
+            method: endPoint.method,
+            encoding: endPoint.encoding
+        ).responseDecodable(of: type, decoder: JSONDecoder.defaultFromSnakeCase) { response in
+            guard response.data != nil else {
+                completion(.failure(NetworkError.invalidData))
+                return
+            }
+            
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - Private -
+    
+    private enum NetworkError: Error {
+        case invalidURL
+        case invalidData
+    }
+}
